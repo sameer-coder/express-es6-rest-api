150dee477409e1d180be67c6f104d0b0ddcaf2c1
Sameer Srivastava <sameer13@gmail.com>
2018-10-23 15:39:54 +0530

    Both apis stable and working

--- src/api/index.js
+++ src/api/index.js
@@ -43,38 +43,50 @@ export default ({ config, db }) => {
 			return;
 		}
 
+		async function fetchAsync(domain_name1) {
+			console.log("EDS Query : https://deliveryindex.org/rest/delivery_index?domain=" + domain_name1);
+			let response = await fetch('https://deliveryindex.org/rest/delivery_index?domain=' + domain_name1);
+			// let response = await fetch('https://api.myjson.com/bins/b5bbo');
+			let data = await response.json();
+			return data;
+		}
+
 		var Company = clearbit.Company;
 		Company.find({ domain: domainName })
-			.then(function (company) {
-				async function fetchAsync() {
-					console.log("EDS Query : https://deliveryindex.org/rest/delivery_index?domain=" + company.domain);
-					let response = await fetch('https://deliveryindex.org/rest/delivery_index?domain=' + company.domain);
-					// let response = await fetch('https://api.myjson.com/bins/b5bbo');
-					let data = await response.json();
-					console.log("just after response-1");
-					return data;
-				}
-
-				fetchAsync()
+			.then((company) => {
+				fetchAsync(company.domain)
 					.then(data => {
-						console.log("just after fetchAsync");
 						let eds_data = data;
 						let eds_raw_string = "Not Available";
 						let gmail_string = "Not Available";
 						let hotmail_string = "Not Available";
 						let yahoo_string = "Not Available";
 
-						if (eds_data !== undefined || eds_data.type !== undefined || eds_data.type === "INVALID_INPUT") {
+						console.log("eds_data:")
+						console.log(eds_data)
+
+						if (eds_data.type !== undefined && eds_data.type === "INVALID_INPUT") {
 							console.log("EDS response : Invalid input, Failed to load delivery index response");
+							eds_raw_string = "Failed to load delivery index";
+							gmail_string = "Failed to load delivery index";
+							hotmail_string = "Failed to load delivery index";
+							yahoo_string = "Failed to load delivery index";
+						} else if (eds_data.message !== undefined && eds_data.message === "Too many requests encountered from IP") {
+							console.log("EDS response : Too many requests encountered from IP");
+							eds_raw_string = "Too many requests from IP";
+							gmail_string = "Too many requests from IP";
+							hotmail_string = "Too many requests from IP";
+							yahoo_string = "Too many requests from IP";
 						} else {
-							eds_raw_string = utf8.encode(JSON.stringify(eds_data.deliverabilityIndexes));
+							let eds_temp = (eds_data.deliverabilityIndexes) + "";
+							eds_raw_string = JSON.stringify(eds_temp.replace(/[^\x00-\x7F]/g, ""));
 							gmail_string = JSON.stringify(eds_data.deliverabilityIndexes[0].ispToIndexMeasurement.gmail.index);
 							hotmail_string = JSON.stringify(eds_data.deliverabilityIndexes[0].ispToIndexMeasurement.hotmail.index);
 							yahoo_string = JSON.stringify(eds_data.deliverabilityIndexes[0].ispToIndexMeasurement.yahoo.index);
 						}
 
 						let tech_stack = (company.tech).join(",");
-						console.log("just after tech_stack");
+						let new_company = JSON.stringify(company);
 
 						const temp_company = {
 							companyid: company.id,
@@ -94,12 +106,14 @@ export default ({ config, db }) => {
 							facebook_likes: company.facebook.likes,
 							twitter: company.twitter.handle,
 							twitter_followers: company.twitter.followers,
-							json_raw: json_raw_string,
-							eds_raw: eds_data_string,
+							json_raw: new_company.replace(/[^\x00-\x7F]/g, "") + "",
+							eds_raw: eds_raw_string,
 							gmail: gmail_string,
 							hotmail: hotmail_string,
 							yahoo: yahoo_string
-						}
+						};
+
+						// res.json({ "status": temp_company });
 
 						dbclient.query('INSERT INTO domain SET ?', temp_company, (err, result) => {
 							if (err) {
@@ -130,19 +144,12 @@ export default ({ config, db }) => {
 						console.log("err-NF" + err); // Company could not be found
 						res.json({ "status": "Error", "Reason": err });
 						return;
-					}).catch((reason) => {
-						console.log("err-reason" + reason);
-						if (!res.headersSent) {
-							res.json({ "status": "Error", "Reason": reason + "" });
-							return;
-						};
-					})
-					.catch(function (err) {
-						console.log('Bad/invalid request, unauthorized, Clearbit error, or failed request');
+					}).catch(function (err) {
+						console.log(err);
 						res.json({ "status": "Error", "Reason": 'Bad/invalid request, unauthorized, Clearbit error, or failed request' });
 						return;
 					});
-			});
+			}).catch(reason => console.log("reason", reason));
 	});
 
 
@@ -201,21 +208,22 @@ export default ({ config, db }) => {
 			return;
 		}
 
-		let arrRoles = [];
-		if (req.query.roles !== undefined || req.query.roles !== "") {
-			arrRoles = req.query.roles.split(","); arrRoles;
+		let arr_seniority = [];
+		if (req.query.roles !== undefined && req.query.roles !== "") {
+			arr_seniority = req.query.seniority.split(",");
 		}
 
-		let roleType = [];
-		if (req.query.roleType == "senior") {
-			roleType = ["ceo", "founder", "president"];
-			arrRoles = roleType;
+		let seniority_type = [];
+		if (req.query.seniority_type == "senior") {
+			seniority_type = ["ceo", "founder", "cto", "director"];
+			arr_seniority = seniority_type;
+			// console.log("arr_seniority is ", arr_seniority);
+			// console.log("typeof arr_seniority is ", typeof(arr_seniority));
 		}
 
-		let total_records = 0;
-		clearbit.Prospector.search({ domain: domainName, roles: arrRoles })
-			.then(function (people) {
-				people.forEach(function (person) {
+		clearbit.Prospector.search({ domain: domainName, seniority: arr_seniority })
+			.then( (people) => {
+				people.forEach( (person) => {
 					// console.log(person);
 					// console.log(person.name.fullName, person.title);
 
@@ -229,6 +237,7 @@ export default ({ config, db }) => {
 						});
 						// let response = await fetch('https://api.myjson.com/bins/b96zg');
 						let data = await response.json();
+						// console.log("Ending sync fetch");
 						return data;
 					}
 
@@ -251,12 +260,12 @@ export default ({ config, db }) => {
 								twitter_followers: person_data.person.twitter.followers,
 								linkedin: person_data.person.linkedin.handle,
 								company: person.company.name,
-								personjson: JSON.stringify(person_data)
+								personjson: JSON.stringify(person_data).replace(/[^\x00-\x7F]/g, "") + ""
 							};
-							// console.log(person);
+							console.log(temp_cust.fullname + temp_cust.seniority + "");
 							xdb.query('INSERT INTO customer SET ?', temp_cust, (err, result) => {
 								if (err) {
-									console.log(err.sqlMessage);
+									console.log(err);
 									if (!res.headersSent) {
 										res.json({ "status": "Error", "Reason": err.sqlMessage });
 										return;
@@ -264,19 +273,18 @@ export default ({ config, db }) => {
 								}
 
 								if (result !== undefined) {
-									total_records++;
-									console.log('Last insert ID-', result.insertId);
-								}
-								if (!res.headersSent) {
-									res.json({ "status": "success" });
+									console.log('Last insert ID -', result.insertId);
+									if (!res.headersSent) {
+										res.json({ "status": "success" });
+										return;
+									}
+								} else {
+									res.json({ "status": "Error", "Reason": "Unknown failure when updating db. Results undefined" });
 									return;
 								}
-
 							});
 						}).catch(reason => console.log(reason))
 				});
-				res.json({ "status": "Error", "Reason": total_records + " records inserted" });
-				return;
 			})
 			.catch(function (err) {
 				console.log('Bad/invalid request, unauthorized, Clearbit error, or failed request');
